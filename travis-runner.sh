cee671ed25858b3966220023b99691523ee49e41
travis-auto <travis>
2016-07-04 19:58:18 +0000

    deployed to github pages

---
+++ travis-runner.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+GH_REPO="@github.com/PierreZ/gwetur.git"
+
+FULL_REPO="https://$GH_TOKEN$GH_REPO"
+
+cp .travis.yml build/bundled/
+cd build/unbundled
+git init
+git config user.name "travis-auto"
+git config user.email "travis"
+
+git add -A
+git commit -m "deployed to github pages"
+git push --force --quiet $FULL_REPO HEAD:gh-pages
